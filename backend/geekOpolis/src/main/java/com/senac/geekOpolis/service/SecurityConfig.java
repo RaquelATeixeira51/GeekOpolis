6dda6fd8ce7cec110923e83d93153d7852f25c75
RaquelATeixeira <85255156+RaquelATeixeira@users.noreply.github.com>
2023-08-18 11:48:48 -0300

    forgot comment

--- backend/geekOpolis/src/main/java/com/senac/geekOpolis/service/SecurityConfig.java
+++ backend/geekOpolis/src/main/java/com/senac/geekOpolis/service/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+// Esta classe cria as instancias do BCryptPasswordEncoder que 
+// podem ser injetadas em outras partes do aplicativo onde a segurança é necessária
 @Configuration
 public class SecurityConfig {
     

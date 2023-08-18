252e67aceae09c6fbfe3606a313b102a615b5015
RaquelATeixeira <85255156+RaquelATeixeira@users.noreply.github.com>
2023-08-18 09:37:08 -0300

    adding initial backend structure

---
+++ backend/geekOpolis/src/test/java/com/senac/geekOpolis/GeekOpolisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.geekOpolis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekOpolisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

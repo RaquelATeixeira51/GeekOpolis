cfe3adec5b1ab38d230c57d10dfe02ecf6ed1deb
ManoelVi <manoelvitorba@gmail.com>
2023-08-30 10:42:35 -0300

    rotas

--- frontend/geekopolis/src/components/Header/index.js
+++ frontend/geekopolis/src/components/Header/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import './index.css';
 import '../../assets/css/reset.css';
 import logo from '../../assets/img/GeekOpolisLogo.png'
-import Login from '../../pages/login';
+import { Link } from "react-router-dom";
 
 function Header(){
     return(
         <div className='header'>
             <img src={logo}/>
             <div className='opcoes'>
-                <h3>Inicio</h3>
-                <h3>Cadastro</h3>
-                <h3>Lista Usuarios</h3>
+                <h3><Link to='/' className='Elementos'>Inicio</Link></h3>
+                <h3><Link to='/CadastroUsuario' className='Elementos'>Cadastro</Link></h3>
+                <h3><Link to='/CadastrarAdmin' className='Elementos'>CadastroAdmin</Link></h3>
             </div>
         </div>
     )
--- frontend/geekopolis/src/index.js
+++ frontend/geekopolis/src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Login from './pages/login';
-import Cadastrar from './pages/Cadastrar';
-import Header from './components/Header';
 import '../src/assets/css/reset.css'
-import Inicio from './pages/Inicio';
 import CadastroUsuario from './pages/Cadastro Usuario';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './pages/login';
+import Inicio from './pages/Inicio';
+import Cadastrar from './pages/Cadastrar';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CadastroUsuario />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Inicio />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/CadastroUsuario' element={<CadastroUsuario />}/>
+        <Route path='/CadastrarAdmin' element={<Cadastrar />}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );

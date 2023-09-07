72d50e5c729173685dac81af36af7bca79b57767
ManoelVi <manoelvitorba@gmail.com>
2023-09-07 19:13:22 -0300

    tela de listagem 1.0

--- frontend/geekopolis/src/index.js
+++ frontend/geekopolis/src/index.js
@@ -5,6 +5,7 @@ import './assets/css/reset.css';
 import './assets/css/root.css';
 import './assets/css/styles.css';
 import Inicio from './pages/Inicio';
+import ListaUsuarios from './pages/ListaUsuarios';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +13,7 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Inicio />} />
+        <Route path='/ListaUsuarios' element={<ListaUsuarios/>}/>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>

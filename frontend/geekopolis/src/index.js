d806fbf394cdc8f2d337add8bea14ff757cd4519
Raquel Aparecida Teixeira <raquelat@ciandt.com>
2023-09-08 00:17:30 -0300

    ajust

--- frontend/geekopolis/src/index.js
+++ frontend/geekopolis/src/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate, useNavigate } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import './assets/css/reset.css';
 import './assets/css/root.css';
@@ -10,44 +10,41 @@ import Login from './pages/login';
 import ListaUsuarios from './pages/ListaUsuarios';
 
 function ProtectedRoute({ element }) {
+  const navigate = useNavigate();
   const [tokenValid, setTokenValid] = useState(true);
 
-  useEffect(() => {
-    const checkTokenValidity = async () => {
-      const token = localStorage.getItem('token');
-      if (!token) {
+  const checkTokenValidity = async () => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      setTokenValid(false);
+      return;
+    }
+
+    try {
+      const response = await fetch(`http://localhost:8080/api/token/valid?token=${localStorage.getItem('token')}`);
+      const isValid = await response.json();
+
+      if (!isValid) {
+        localStorage.removeItem('token');
         setTokenValid(false);
-        return;
+        navigate('/');
       }
+    } catch (error) {
+      console.error('Erro ao verificar a validade do token', error);
+    }
+  };
 
-      try {
-        const response = await fetch(`http://localhost:8080/api/token/valid?token=${localStorage.getItem('token')}`);
-        const isValid = await response.json();
-
-        if (!isValid) {
-          localStorage.removeItem('token');
-          setTokenValid(false);
-
-        }
-      } catch (error) {
-        console.error('Erro ao verificar a validade do token', error);
-      }
-    };
+  useEffect(() => {
+    const tokenCheckInterval = setInterval(checkTokenValidity, 10 * 60 * 1000);
 
     checkTokenValidity();
-
-    const tokenCheckInterval = setInterval(checkTokenValidity, 10 * 60 * 1000);
 
     return () => {
       clearInterval(tokenCheckInterval);
     };
-  }, []);
+  }, [navigate]);
 
-  if (!tokenValid) {
-    return <Navigate to="/" />;
-  }
-
-  return element;
+  return tokenValid ? element : <Navigate to="/" />;
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

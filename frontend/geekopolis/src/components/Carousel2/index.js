666c658b765f7ac5846de7c4c11738ab0fe17f97
Raquel Aparecida Teixeira <raquelat@ciandt.com>
2023-09-28 16:12:09 -0300

    slider functioning

--- frontend/geekopolis/src/components/Carousel2/index.js
+++ frontend/geekopolis/src/components/Carousel2/index.js
@@ -21,50 +21,13 @@ import './index.css';
 
 function Carousel2(props) {
   const { products } = props;
-  const [body, setBody] = React.useState({
-    nome: '',
-    avaliacao: 0,
-    descricao: '',
-    preco: 0, 
-    qtdEstoque: 0, 
-    imagesPath: 0,
-    categoriaId: 0,
-    status: false,
-  });
-
-  const handleBody = (e) => {
-    const newBody = { ...body, [e.target.name]: e.target.value };
-    if (e.target.name === 'categoriaId') {
-      newBody.categoriaId = parseInt(e.target.value, 10);
-    }
-    setBody(newBody);
-  };
 
   return (
     <>
       <div className="carousel-container">
-          {/* <Slider
-            infinite={true}
-            speed="500"
-            slidesToShow={1}
-            slidesToScroll={1}
-            arrows={true}
-          >
-            {products.map((product) => (
-              <div className="carousel-slide">
-                <div className="inside2">
-                  <img
-                    src={product.imagesPath[0]}
-                    className="image-product"
-                    alt={product.nome}
-                  />
-                </div>
-              </div>
-            ))}
-          </Slider> */}
           <Slider infinite={true} speed='500' slidesToShow={1} slidesToScroll={1} arrows={true}>
-              {Array.isArray(body.imagesPath) ? (
-                body.imagesPath.map((image, index) => (
+              {Array.isArray(products) ? (
+                products.map((image, index) => (
                   <div className="carousel-slide">
                     <div className="inside2">
                     <img src={image} alt={`Image ${index}`} className="carousel-image" />

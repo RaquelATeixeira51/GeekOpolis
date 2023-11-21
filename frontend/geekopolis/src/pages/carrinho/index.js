04c7655907d2de99380cf758fb2da4f0f84334b1
EndriOliveira <endriojesus76@gmail.com>
2023-11-21 17:14:39 -0300

    Fix: Tela de carrinho

--- frontend/geekopolis/src/pages/carrinho/index.js
+++ frontend/geekopolis/src/pages/carrinho/index.js
@@ -74,6 +74,30 @@ export default function Carrinho() {
       });
   };
 
+  const handleFirstFreight = (cepParam, totalProdutos) => {
+    fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.REACT_APP_GOOGLE_MAPS_KEY}&address=${cepParam}`,
+      {
+        method: 'GET',
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const { lat, lng } = data.results[0].geometry.location;
+        setTotal(
+          !valorFrete || cep !== cepParam
+            ? Number(getDistanceFromLatLonInKm(lat, lng) * 1.5) +
+                Number(totalProdutos)
+            : total
+        );
+        setCep(cepParam);
+        setValorFrete(Number(getDistanceFromLatLonInKm(lat, lng) * 1.5));
+      })
+      .catch(() => {
+        makeToast('error', 'Erro ao calcular frete');
+      });
+  };
+
   const calcularFrete = () => {
     if (!cepRef.current.value) {
       makeToast('error', 'Digite um CEP válido');
@@ -92,7 +116,15 @@ export default function Carrinho() {
     )
       .then((response) => response.json())
       .then((data) => {
+        console.log(data);
         const { lat, lng } = data.results[0].geometry.location;
+        setEndereco({
+          ...endereco,
+          cep: cepRef.current.value,
+          logradouro: data.results[0].address_components[1].long_name,
+          cidade: data.results[0].address_components[3].long_name,
+          uf: data.results[0].address_components[4].short_name,
+        });
         setTotal(
           !valorFrete || cep !== cepRef.current.value
             ? Number(getDistanceFromLatLonInKm(lat, lng) * 1.5) +
@@ -102,8 +134,9 @@ export default function Carrinho() {
         setCep(cepRef.current.value);
         setValorFrete(Number(getDistanceFromLatLonInKm(lat, lng) * 1.5));
       })
-      .catch(() => {
-        makeToast('error', 'Erro ao buscar endereços');
+      .catch((err) => {
+        console.log(err);
+        makeToast('error', 'Erro ao calcular frete');
       });
   };
 
@@ -134,8 +167,9 @@ export default function Carrinho() {
       .then((response) => response.json())
       .then((data) => {
         if (data.length > 0) {
-          setEndereco(data.find((e) => e.principal === true));
-          calcularFrete();
+          const endereco = data.find((e) => e.principal === true);
+          setEndereco(endereco);
+          handleFirstFreight(endereco.cep, cart.total);
         }
       })
       .catch(() => {
@@ -211,32 +245,37 @@ export default function Carrinho() {
             </div>
 
             <div className="cart-address">
-              <h2>CEP:</h2>
-              <div className="cart-address-info">
-                <input
-                  type="text"
-                  id="email"
-                  ref={cepRef}
-                  className="cart-rounded-input"
-                  value={endereco?.cep ? endereco?.cep : null}
-                />
-                <button
-                  type="button"
-                  className="cart-address-change"
-                  onClick={calcularFrete}
-                >
-                  Calcular Frete
-                </button>
+              <h2>CEP: {endereco?.cep}</h2>
+              <h2>Logradouro: {endereco?.logradouro}</h2>
+              <h2>Cidade: {endereco?.cidade}</h2>
+              <h2>UF: {endereco?.uf}</h2>
+              <h2>Numero: {endereco?.numero}</h2>
+              <h2>Complemento: {endereco?.complemento}</h2>
+
+              <div className="cart-address-change">
+                <Link to="/editarEndereco">Alterar Endereço de entrega</Link>
               </div>
             </div>
 
-            <h2 className="cart-total">Total: R$ {total}</h2>
+            <h3 className="cart-subtotal cart-total">
+              Subtotal: R$ {totalProdutos}{' '}
+            </h3>
+            <h3 className="cart-subtotal cart-total">
+              Frete: R${' '}
+              {valorFrete?.toLocaleString('pt-BR', { currency: 'BRL' })}{' '}
+            </h3>
+            <h2 className="cart-total">
+              Total: R$ {total?.toLocaleString('pt-BR', { currency: 'BRL' })}
+            </h2>
             <Link to="/checkout">
-              <button type="button" onClick={checkout} className="cart-address-change">
-                checkout
+              <button
+                type="button"
+                onClick={checkout}
+                className="cart-address-change"
+              >
+                Checkout
               </button>
             </Link>
-            
           </div>
         </main>
       </div>
